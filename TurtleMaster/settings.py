28792e33627302c62cde0b8143cd23c7a8a41655
Zhigang Wang <zhigangw@hotmail.com>
2020-04-13 19:53:19 -0700

    update settings.py

--- TurtleMaster/settings.py
+++ TurtleMaster/settings.py
@@ -79,10 +79,10 @@ DATABASES = {
 #        'ENGINE': 'django.db.backends.sqlite3',
 #        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
         'ENGINE': 'django.db.backends.postgresql',
-        'NAME': os.environ.get('DBNAME', ''),
+        'NAME': os.environ.get('DBNAME', 'turtle_master_db'),
         'HOST': os.environ.get('DBHOST', 'localhost'),
-        'USER': os.environ.get('DBUSER', ''),
-        'PASSWORD': os.environ.get('DBPASS', '')
+        'USER': os.environ.get('DBUSER', 'turtle_master_db_admin'),
+        'PASSWORD': os.environ.get('DBPASS', 'covidhub@io')
     }
 }
 
