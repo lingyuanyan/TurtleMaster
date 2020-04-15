8a9f0dfb7bc5b216998116b4b1fcce6a0206d291
zhigangw <zhigangw@hotmail.com>
2020-04-15 04:04:16 +0000

    add more server name in nginx conf; fix some minor issues in env.sh

--- env.sh
+++ env.sh
@@ -3,17 +3,19 @@ export DBUSER="turtle_master_db_admin"
 export DBNAME="turtle_master_db"
 export DBPASS="covidhub@io"
 
-echo Please make sure the database $DBNAME has been created on $DBHOSTi
+echo Please make sure the database $DBNAME has been created on $DBHOST
 echo and db user $DBUSER has been set with pwd = $DBPASS
 echo Please run the following SQL to do so:
 echo sudo -u postgres psql
-echo CREATE DATABASE $DBNAME ;
-echo CREATE ROLE $DBUSER WITH ENCRYPTED PASSWORD $DBPASS;
-echo ALTER ROLE $DBUSER WITH LOGIN SUPERUSER INHERIT CREATEDB CREATEROLE REPLICATION;
-echo GRANT ALL PRIVILEGES ON DATABASE $DBNAME TO  $DBUSER;
-echo ALTER ROLE $DBUSER SET client_encoding TO 'utf8';
-echo ALTER ROLE $DBUSER SET default_transaction_isolation TO 'read committed';
-echo ALTER ROLE $DBUSER SET timezone TO 'UTC';
+echo CREATE DATABASE $DBNAME \;
+echo CREATE ROLE $DBUSER WITH ENCRYPTED PASSWORD \'$DBPASS\'\;
+echo ALTER ROLE $DBUSER WITH LOGIN SUPERUSER INHERIT CREATEDB CREATEROLE REPLICATION\;
+echo GRANT ALL PRIVILEGES ON DATABASE $DBNAME TO  $DBUSER\;
+echo ALTER ROLE $DBUSER SET client_encoding TO 'utf8'\;
+echo ALTER ROLE $DBUSER SET default_transaction_isolation TO 'read committed'\;
+echo ALTER ROLE $DBUSER SET timezone TO 'UTC'\;
+
+python manage.py collectstatic
 
 sudo rm /etc/nginx/sites-enabled/django_nginx.conf
 sudo ln -s $PWD/django_nginx.conf /etc/nginx/sites-enabled/
