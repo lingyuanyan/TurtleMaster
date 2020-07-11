d07c99b19e6f8057730a442995efd0e0b79b22a4
Lingyuan Yan <lingyuanyan@hotmail.com>
2020-07-11 15:44:16 -0700

    Js

---
+++ TurtleMasterApp/static/TurtleMasterApp/TurtleMaster.js
@@ -0,0 +1,33 @@
+var banner_vue = new Vue({
+  delimiters: ['[[', ']]'],
+  el: '#banner',
+  data: {
+  },
+});
+var topline_vue = new Vue({
+  delimiters: ['[[', ']]'],
+  el: '#topline',
+  data: {
+    topline_json:topline_json,
+  },
+});
+var data_list_vue = new Vue({
+  delimiters: ['[[', ']]'],
+  el: '#data-list',
+  data: {
+    us_satistics_json: us_satistics_json,
+    world_statistics_json: world_statistics_json,
+  },
+});
+var infomation_hub_vue = new Vue({
+  delimiters: ['[[', ']]'],
+  el: '#information-hub',
+  data: {
+  },
+});
+var footer_vue = new Vue({
+  delimiters: ['[[', ']]'],
+  el: '#footer',
+  data: {
+  },
+});
