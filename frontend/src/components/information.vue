5f331e2b342e384ae8fac0be2e462b328c0c3a5e
AlexCao-html <caobubo@gmail.com>
2020-10-25 19:59:27 -0700

    hello

--- frontend/src/components/information.vue
+++ frontend/src/components/information.vue
@@ -1,160 +1,152 @@
 <template>
-<<<<<<< HEAD
-  <div class="information.vue">
+    <<<<<<< HEAD
+    <div class="information.vue">
 
-<h3>Tips to stop the spread of COVID-19</h3>
-<ul>
-Keep 6 feet of distance (2 yards, 1.8288)
-Stay inside unless you have to go out for a very important reason. when you do, make shure not to touch 
-What to do if your infected
-Contact with people virtual
-Quarantine
-Tell people and get help virtually (doctors)
-Hope for the best and prepare for your doom
-</ul>
-How to avoid getting Infected
-Limit outside activity
-Wear a mask if you must go outside
-Try to social distance with people that are not in your family.
-Never enter crowded spaces with other people that are not you family.
-WASH YOUR HANDS using soap and water.
-Travel at the risk of infection and most likely a painful death
-Wear a facemask as they are proven to help reduce far air particles go when you cough or sneeze.
-Wash your mask daily
-Avoid believing that Covid is the same as flu because if you are infected with flu after you infect people 56 people will get sick before that strand is dead.  While with Covid you can infect more than 2047 people by just getting infected.
-If you go a restaurant avoid seating in a section with air conditioning. This is because the Air conditioning can circulate Covid particles if someone sitting in the air circulation area has covid. Then they can spread covid to you because the air conditioner doesn’t let the air stop.
-First day fells like the flu.
-Day 5 trouble breathing
-Seven days to go to hospital.
-Some will recover after 7 days.
-Others will go to the hospital
-If you go to the hospital you will probably have Pneumonia.
-<h3>What happens after you have been Infected</h3>
-<ol>
-<li>1-2 sour throat </li>
-<li>3 throat painful raised body temp </li>
-<li> 4 throat pain more. 35-37 Celsius </li>
-<li>dry cough tired higher temp </li>
-<li>Temp: 38c, serious headaches, and diarrhea</li>
-<li>8-9 worsens. </li>
-<li>short breath </li>
-<li>38 c </li>
-<li>10-11 lost appetite immediate treatment  </li>
-<li>13 most less serious </li>
-<li>15 cardiac injury </li>
-<li>18 DEATH weak patients’ reinfection </li>
-<li>after wads discharged and healed</li>
-</ol>
-then soap
-https://www.youtube.com/watch?v=U8r3oTVMtQ0
-Also, soap is easier to get it completely covered
-cover when sneeze or coughing
-Correction. Using hand sanitizer first
-then soap
-avoid touching Items around the
-clean often
-to kill most viruses
-What better soap or hand sanitizer. IT says that in general soap is better. This is because they keep the cell completely apart
-You and objects
-https://www.youtube.com/watch?v=x7KKkElpyKQ&t=2s
-but hand sanitizer is a faster solution for when you are in a hurry
-soap is better because it also gets rid of dirt that could be hiding but hand sanitizer is a faster solution for when you are in a hurry. Hand Sanitizers are also very effective.
-Symptoms
-Fever or chills
-Cough
-Shortness of breath or difficulty breathing
-Fatigue
-Muscle or body aches
-Headache
-New loss of taste or smell
-Sore throat
-Congestion or runny nose
-Nausea or vomiting
-Diarrhea
-Wash your hands often with soap and water for at least 20 seconds, especially after blowing your nose, coughing, or sneezing; going to the bathroom; and before eating or preparing food.
-Avoid touching your eyes, nose, and mouth with unwashed hands.
-Stay home when you are sick.
-Cover your cough or sneeze with a tissue, then throw the tissue in the trash
-Quarintine if u have done these things with someone who has covid in the last 3 months
-You were within 6 feet of someone who has COVID-19 for a total of 15 minutes or more
-You provided care at home to someone who is sick with COVID-19
-You had direct physical contact with the person (hugged or kissed them)
-You shared eating or drinking utensils
-They sneezed, coughed, or somehow got respiratory droplets on you
-Trouble breathing
-Persistent pain or pressure in the chest
-New confusion
-Inability to wake or stay awake
-Bluish lips or face
-Pneumonia
-Wear face masks
-cotton x2 is best kind of face mask.
-cotton x2 is best kind of face mask.
-Avoid social activities in person if you are or a loved one is classified as at risk
-Use both hand sanitizer and soap and water when possible.
-When on the go use hand sanitizer for a long as possible rubbing it whole hand.
-When using soap and water also cover your whole hand and nails. 
-Do both for 20 seconds.
-When using hand sanitizer use it until your hand are dry.
-Cove your sneeze with you elbow
-Do not touch your face because your eyes nose and mouth because they are like the one point on the death star that is week.
-To clean your hands the best use both hand sanitizer and soap and water. This is because Hand sanitizer will get rid of Rhinovirus which is stronger than the shell of covid. So, Hand sanitizer will get rid of most of the germs. Then if you use soap and water this will get rid of the other germs that the hand sanitizer did not get.
-Your lungs can get infected. When this happens
-The cases that are reported are the people that hwere tested not everyone is being tested daily.
-=======
-  <div class="information">
-    <section class="videos">
-      <h1>Videos</h1>
-      <a href="https://www.youtube.com/watch?v=n6QwnzbRUyA" target="_blank">How to lower your chances of getting Covid. </a><br/>
-      <a href="https://www.youtube.com/watch?v=FVIGhz3uwuQ" target="_blank">Covid vs Flu (Hint: there very diffrent. ) </a><br/>
-      <a href="https://www.youtube.com/watch?v=-LKVUarhtvE" target="_blank">soap vs covid.</a><br/>
-      <a href="https://www.youtube.com/watch?v=O-3Mlj3MQ_Q" target="_blank">Misleading data on Covid. </a><br/>
-      <a href="https://www.youtube.com/watch?v=U8r3oTVMtQ0" target="_blank">Symptoms of Covid</a><br/>
-      <a href="https://www.youtube.com/watch?v=x7KKkElpyKQ" target="_blank">soap vs. hand sanitizer</a>
-    </section>
->>>>>>> 52f97e3ca6f03bad7e52f2f46efe0be4e7f7371b
-  </div>
+        <h3>Tips to stop the spread of COVID-19</h3>
+        <ul>
+            Keep 6 feet of distance (2 yards, 1.8288)
+            Stay inside unless you have to go out for a very important reason. when you do, make shure not to touch
+            What to do if your infected
+            Contact with people virtual
+            Quarantine
+            Tell people and get help virtually (doctors)
+            Hope for the best and prepare for your doom
+        </ul>
+        How to avoid getting Infected
+        Limit outside activity
+        Wear a mask if you must go outside
+        Try to social distance with people that are not in your family.
+        Never enter crowded spaces with other people that are not you family.
+        WASH YOUR HANDS using soap and water.
+        Travel at the risk of infection and most likely a painful death
+        Wear a facemask as they are proven to help reduce far air particles go when you cough or sneeze.
+        Wash your mask daily
+        Avoid believing that Covid is the same as flu because if you are infected with flu after you infect people 56 people will get sick before that strand is dead.  While with Covid you can infect more than 2047 people by just getting infected.
+        If you go a restaurant avoid seating in a section with air conditioning. This is because the Air conditioning can circulate Covid particles if someone sitting in the air circulation area has covid. Then they can spread covid to you because the air conditioner doesn’t let the air stop.
+        First day fells like the flu.
+        Day 5 trouble breathing
+        Seven days to go to hospital.
+        Some will recover after 7 days.
+        Others will go to the hospital
+        If you go to the hospital you will probably have Pneumonia.
+        <h3>What happens after you have been Infected</h3>
+        <ol>
+            <li>1-2 sour throat </li>
+            <li>3 throat painful raised body temp </li>
+            <li> 4 throat pain more. 35-37 Celsius </li>
+            <li>dry cough tired higher temp </li>
+            <li>Temp: 38c, serious headaches, and diarrhea</li>
+            <li>8-9 worsens. </li>
+            <li>short breath </li>
+            <li>38 c </li>
+            <li>10-11 lost appetite immediate treatment  </li>
+            <li>13 most less serious </li>
+            <li>15 cardiac injury </li>
+            <li>18 DEATH weak patients’ reinfection </li>
+            <li>after wads discharged and healed</li>
+        </ol>
+        then soap
+        https://www.youtube.com/watch?v=U8r3oTVMtQ0
+        Also, soap is easier to get it completely covered
+        cover when sneeze or coughing
+        Correction. Using hand sanitizer first
+        then soap
+        avoid touching Items around the
+        clean often
+        to kill most viruses
+        What better soap or hand sanitizer. IT says that in general soap is better. This is because they keep the cell completely apart
+        You and objects
+        https://www.youtube.com/watch?v=x7KKkElpyKQ&t=2s
+        but hand sanitizer is a faster solution for when you are in a hurry
+        soap is better because it also gets rid of dirt that could be hiding but hand sanitizer is a faster solution for when you are in a hurry. Hand Sanitizers are also very effective.
+        Symptoms
+        Fever or chills
+        Cough
+        Shortness of breath or difficulty breathing
+        Fatigue
+        Muscle or body aches
+        Headache
+        New loss of taste or smell
+        Sore throat
+        Congestion or runny nose
+        Nausea or vomiting
+        Diarrhea
+        Wash your hands often with soap and water for at least 20 seconds, especially after blowing your nose, coughing, or sneezing; going to the bathroom; and before eating or preparing food.
+        Avoid touching your eyes, nose, and mouth with unwashed hands.
+        Stay home when you are sick.
+        Cover your cough or sneeze with a tissue, then throw the tissue in the trash
+        Quarintine if u have done these things with someone who has covid in the last 3 months
+        You were within 6 feet of someone who has COVID-19 for a total of 15 minutes or more
+        You provided care at home to someone who is sick with COVID-19
+        You had direct physical contact with the person (hugged or kissed them)
+        You shared eating or drinking utensils
+        They sneezed, coughed, or somehow got respiratory droplets on you
+        Trouble breathing
+        Persistent pain or pressure in the chest
+        New confusion
+        Inability to wake or stay awake
+        Bluish lips or face
+        Pneumonia
+        Wear face masks
+        cotton x2 is best kind of face mask.
+        cotton x2 is best kind of face mask.
+        Avoid social activities in person if you are or a loved one is classified as at risk
+        Use both hand sanitizer and soap and water when possible.
+        When on the go use hand sanitizer for a long as possible rubbing it whole hand.
+        When using soap and water also cover your whole hand and nails.
+        Do both for 20 seconds.
+        When using hand sanitizer use it until your hand are dry.
+        Cove your sneeze with you elbow
+        Do not touch your face because your eyes nose and mouth because they are like the one point on the death star that is week.
+        To clean your hands the best use both hand sanitizer and soap and water. This is because Hand sanitizer will get rid of Rhinovirus which is stronger than the shell of covid. So, Hand sanitizer will get rid of most of the germs. Then if you use soap and water this will get rid of the other germs that the hand sanitizer did not get.
+        Your lungs can get infected. When this happens
+        The cases that are reported are the people that hwere tested not everyone is being tested daily.
+        =======
+        <section class="videos">
+            <h1>Videos</h1>
+            <a href="https://www.youtube.com/watch?v=n6QwnzbRUyA" target="_blank">How to lower your chances of getting Covid. </a><br />
+            <a href="https://www.youtube.com/watch?v=FVIGhz3uwuQ" target="_blank">Covid vs Flu (Hint: there very diffrent. ) </a><br />
+            <a href="https://www.youtube.com/watch?v=-LKVUarhtvE" target="_blank">soap vs covid.</a><br />
+            <a href="https://www.youtube.com/watch?v=O-3Mlj3MQ_Q" target="_blank">Misleading data on Covid. </a><br />
+            <a href="https://www.youtube.com/watch?v=U8r3oTVMtQ0" target="_blank">Symptoms of Covid</a><br />
+            <a href="https://www.youtube.com/watch?v=x7KKkElpyKQ" target="_blank">soap vs. hand sanitizer</a>
+        </section>
+        >>>>>>> 52f97e3ca6f03bad7e52f2f46efe0be4e7f7371b
 </template>
-
 <script>
-export default {
-  name: 'Information.vue',
-  props: {
-    msg: String
-  }
-}
+    export default {
+        name: 'Information.vue',
+        props: {
+            msg: String
+        }
+    }
 </script>
 
 <!-- Add "scoped" attribute to limit CSS to this component only -->
 <style scoped>
-  section.videos a{
-    display: block;
-    text-decoration: none;
-    color: #8080ff;
-    transition: all 0.3s cubic-bezier(0.93, 0.43, 0.66, 0.29);
-    -webkit-transition: all 0.3s cubic-bezier(0.93, 0.43, 0.66, 0.29);
-    -moz-transition: all 0.3s cubic-bezier(0.93, 0.43, 0.66, 0.29);
-    -o-transition: all 0.3s cubic-bezier(0.93, 0.43, 0.66, 0.29);
-    -ms-transition: all 0.3s cubic-bezier(0.93, 0.43, 0.66, 0.29);
-    background-color: #000;
-    margin: 5px;
-    padding: 1vh 1vw;
-    border-radius: 50%;
-    -webkit-border-radius: 50%;
-    -moz-border-radius: 50%;
-    -o-border-radius: 50%;
-    -ms-border-radius: 50%;
-    text-align: center;
-    width: 95vw;
-    height: 5vh;
-  }
+    section.videos a {
+        display: block;
+        text-decoration: none;
+        color: #8080ff;
+        transition: all 0.3s cubic-bezier(0.93, 0.43, 0.66, 0.29);
+        -webkit-transition: all 0.3s cubic-bezier(0.93, 0.43, 0.66, 0.29);
+        -moz-transition: all 0.3s cubic-bezier(0.93, 0.43, 0.66, 0.29);
+        -o-transition: all 0.3s cubic-bezier(0.93, 0.43, 0.66, 0.29);
+        background-color: #000;
+        margin: 5px;
+        padding: 1vh 1vw;
+        border-radius: 50%;
+        -webkit-border-radius: 50%;
+        -moz-border-radius: 50%;
+        text-align: center;
+        width: 95vw;
+        height: 5vh;
+    }
 
-  section.videos a:hover{
-    color: #bfbfff;
-    border-radius: 0;
-    -webkit-border-radius: 0;
-    -moz-border-radius: 0;
-    -o-border-radius: 0;
-    -ms-border-radius: 0;
-  }
+        section.videos a:hover {
+            color: #bfbfff;
+            border-radius: 0;
+            -webkit-border-radius: 0;
+            -moz-border-radius: 0;
+        }
 </style>
