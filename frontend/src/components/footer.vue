d4d19295d9ca7cebd90e0a99b818312187fbd9a8
AlexCao-html <caobubo@gmail.com>
2020-11-23 10:41:40 -0800

    I edited footer.vue and I added a meta in index.html in frontend/public while I pressed Ctrl-S on some stuff so it said that I edited it

--- frontend/src/components/footer.vue
+++ frontend/src/components/footer.vue
@@ -1,5 +1,6 @@
 <template>
   <div class="footer">
+    <img alt="TurtleMaster Logo" class="game-link" />Here is a game you can play <a href="">Monster Slayer</a>
   </div>
 </template>
 
@@ -14,13 +15,24 @@ export default {
 
 <!-- Add "scoped" attribute to limit CSS to this component only -->
 <style scoped>
-footer{
+@import url('https://fonts.googleapis.com/css2?family=Dancing+Script&family=Great+Vibes&display=swap');
+.footer{
   background: #20f030;
-  border:1px solid #008000;
-  font-family: cursive;
-  position:fixed;
+  border: 5px groove #38f576;
+  font-family: 'Dancing Script', 'Great Vibes', cursive, fantasy, serif, monospace, sans-serif;
+  position: absolute;
   bottom: 0px;
   width: 100%;
   left: 0px;
+}
+
+.game-link {
+  color: #4040ff;
+  text-decoration: none;
+}
+
+.game-link:hover, .game-link:focus {
+  color: #0000bf;
+  text-decoration: underline;
 }
 </style>
