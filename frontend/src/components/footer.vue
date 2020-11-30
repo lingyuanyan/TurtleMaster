fcc995bda2aaac0e6c5d45136b89ddfb8794d269
AlexCao-html <caobubo@gmail.com>
2020-11-30 10:24:21 -0800

    fixed border in footer

--- frontend/src/components/footer.vue
+++ frontend/src/components/footer.vue
@@ -18,12 +18,15 @@ export default {
 @import url('https://fonts.googleapis.com/css2?family=Dancing+Script&family=Great+Vibes&display=swap');
 .footer{
   background: #20f030;
-  border: 5px groove #5eac64;
+  border: 2.5px solid #4db956;
   font-family: 'Dancing Script', 'Great Vibes', cursive, serif, monospace, sans-serif;
   width: 100%;
   position: fixed;
   bottom: 0px;
   left: 0px;
   text-align: center;
+  box-sizing: border-box;
+  -webkit-box-sizing: border-box;
+  -moz-box-sizing: border-box;
 }
 </style>
