c7d32ff3cc6244dcec09b0700080858edd15b138
Zhigang Wang <zhigangw@hotmail.com>
2022-12-02 21:38:33 -0800

    add bootstrap files

--- frontend/vue.config.js
+++ frontend/vue.config.js
@@ -1,16 +1,23 @@
 // frontend/vue.config.js
 module.exports = {
-    devServer: {
-      proxy: {
-        '^/api/': {
-          target: 'http://127.0.0.1:5000/',
-          ws: true,
-          changeOrigin: true
-        }
+  devServer: {
+    proxy: {
+      '^/api/': {
+        target: 'http://127.0.0.1:5000/',
+        ws: true,
+        changeOrigin: true
       }
-    },
-    // outputDir must be added to Django's TEMPLATE_DIRS
-    outputDir: './dist/',
-    // assetsDir must match Django's STATIC_URL
-    assetsDir: 'static',
+    }
+  },
+  // outputDir must be added to Django's TEMPLATE_DIRS
+  outputDir: './dist/',
+  // assetsDir must match Django's STATIC_URL
+  assetsDir: 'static',
+  css: {
+    loaderOptions: {
+      sass: {
+        additionalData: `@import "~@/assets/styles/main.scss";`
+      }
+    }
   }
+}
